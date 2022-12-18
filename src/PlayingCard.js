2698059d74491ff0272db20211be0cabfcc5e616
Sasha Czerniawski <sashacandoit@gmail.com>
2022-12-18 12:26:16 -0500

    useFlip hook added to PlayingCard

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
+import useFlip from './hooks/useFlip'
 import "./PlayingCard.css"
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, toggleFlip] = useFlip(true);
   return (
     <img
       src={isFacingUp ? front : back}
       alt="playing card"
-      onClick={flipCard}
+      onClick={toggleFlip}
       className="PlayingCard Card"
     />
   );
