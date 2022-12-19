d7f8b4c64cb0da2e021f808cab40858b5b5af836
Sasha Czerniawski <sashacandoit@gmail.com>
2022-12-19 14:44:59 -0500

    added helper functions and found bug in PlayingCardList

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,28 +1,30 @@
 import React from "react";
 import useAxios from "./hooks/useAxios";
+import { formatCard } from "./helpers";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
-function CardTable() {
-//   const [cards, setCards] = useState([]);
-    const [cards, addCards] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/")
-  return (
-    <div className="PlayingCardList">
-      <h3>Pick a card, any card!</h3>
-      <div>
-        <button onClick={addCards}>Add a playing card!</button>
-      </div>
-      <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
-        ))}
-      </div>
-    </div>
-  );
+function PlayingCardList() {
+    //   const [cards, setCards] = useState([]);
+    const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/")
+
+    return (
+        <div className="PlayingCardList">
+            <h3>Pick a card, any card!</h3>
+            <div>
+                <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
+            </div>
+            <div className="PlayingCardList-card-area">
+                {cards.map(cardData => (
+                    <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-CardTable.defaultProps = {};
+// PlayingCardList.defaultProps = {};
 
-export default CardTable;
+export default PlayingCardList;
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -7,13 +7,13 @@ import axios from "axios";
  * The first element is an array of data obtained from previous AJAX requests (since we will add to this array, it’s a piece of state). 
  * The second element is a function that will add a new object of data to our array. */
 
-const useAxios = (baseUrl, restOfUrl="") => {
+const useAxios = (baseUrl) => {
     const [responses, setResponses] = useState([]);
 
-    const addResData = async () => {
+    const addResData = async (formatter = data => data, restOfUrl="") => {
         const res = await axios.get(`${baseUrl}${restOfUrl}`);
         console.log(res)
-        setResponses(data => [...data, { ...res.data, id: uuid() }]);
+        setResponses(data => [...data, formatter(res.data)]);
     };
     return [responses, addResData]
 }
